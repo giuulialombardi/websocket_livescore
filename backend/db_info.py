cbf4deaea27a9ecc5f17e899f5943737af89d3ca
giulia lombardi <giulia.lombardi@fermi.mo.it>
2026-01-16 21:57:23 +0100

    aggiunti file per docker

--- backend/db_info.py
+++ backend/db_info.py
@@ -1,4 +1,6 @@
-MONGO_URL="mongodb://localhost:27017"
-DB_NAME="livescore"
-COLLECTION_NAME1="piloti"
-COLLECTION_NAME2="gare"
+import os
+
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+DB_NAME = "livescore"
+COLLECTION_NAME1 = "piloti"
+COLLECTION_NAME2 = "gare"
